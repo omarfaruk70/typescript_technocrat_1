d08a42c23fbfe018f8a7eba5f87f1d704b96680a
omarfaruk70 <faruk1611281892@gmail.com>
2024-11-04 22:42:41 +0600

    added keyof operator

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -10,8 +10,22 @@
   type Owner = "Bike" | "Car" | "Ship"; // manually
   //   using keyof operator               // Dynamically
   type Owner2 = keyof Vehicles;
-  //   const personl:Owner = "Bike"
+    // const personl:Owner = "Car"
 
-  const getPropertyValue = () => {};
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key:Y) => {
+    return obj[key]
+  };
+  const user1 = {
+    name: "MR. X",
+    age: 26,
+    address: "Dhaka, Bangladesh"
+  };
+  const car1 = {
+    model: "X corolla",
+    year: 1292,
+    cc: 1500,
+  };
+  const result1 = getPropertyValue(user1, "name")   // executed because name property is exist user1
+  //const result2 = getPropertyValue(user1, "audi")   // error! because audi property isn't exist car1
   //
 }
